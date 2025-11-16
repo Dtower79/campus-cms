0461a568f182e026fe7953aed8cb4bdc9801d353
Raul Nicolas <dtower79@gmail.com>
2025-11-16 19:55:41 +0100

    Modelatge complet de Curs, Modul i Components de Quiz

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,3 +1,33 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface QuizOpcio extends Struct.ComponentSchema {
+  collectionName: 'components_quiz_opcio';
+  info: {
+    displayName: 'Opci\u00F3';
+  };
+  attributes: {
+    esCorrecta: Schema.Attribute.Boolean;
+    text: Schema.Attribute.String;
+  };
+}
+
+export interface QuizPregunta extends Struct.ComponentSchema {
+  collectionName: 'components_quiz_preguntas';
+  info: {
+    displayName: 'Pregunta';
+  };
+  attributes: {
+    explicacio: Schema.Attribute.Blocks;
+    opcions: Schema.Attribute.Component<'quiz.opcio', true>;
+    text: Schema.Attribute.String;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'quiz.opcio': QuizOpcio;
+      'quiz.pregunta': QuizPregunta;
+    }
+  }
+}
