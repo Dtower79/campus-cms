9355e409e31983929152691d39b3dd81e3553050
Raul Nicolas <dtower79@gmail.com>
2025-11-24 01:35:22 +0100

    Feat: Nombre y apellidos en User

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -1053,6 +1053,7 @@ export interface PluginUsersPermissionsUser
     draftAndPublish: false;
   };
   attributes: {
+    apellidos: Schema.Attribute.String;
     blocked: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
     confirmationToken: Schema.Attribute.String & Schema.Attribute.Private;
     confirmed: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
@@ -1074,6 +1075,7 @@ export interface PluginUsersPermissionsUser
       'oneToMany',
       'api::matricula.matricula'
     >;
+    nombre: Schema.Attribute.String;
     notificacions: Schema.Attribute.Relation<
       'oneToMany',
       'api::notificacion.notificacion'
