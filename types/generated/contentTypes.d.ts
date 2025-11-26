033a8e68d04c21332afbdeb4dceed0eb6b7e9ccf
Raul Nicolas <dtower79@gmail.com>
2025-11-26 17:20:30 +0100

    Añadido campo profesor y fix plugins

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -1134,6 +1134,7 @@ export interface PluginUsersPermissionsUser
       Schema.Attribute.SetMinMaxLength<{
         minLength: 6;
       }>;
+    es_professor: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
       'oneToMany',
