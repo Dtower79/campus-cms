d9c2b94d074c638f52ec0b6cc0f62f90d2294e62
Raul Nicolas <dtower79@gmail.com>
2025-11-26 17:17:49 +0100

    Configuracion Cloudinary TS

--- config/plugins.ts
+++ config/plugins.ts
@@ -8,27 +8,9 @@ export default ({ env }) => ({
         api_secret: env('CLOUDINARY_SECRET'),
       },
       actionOptions: {
-        upload: {},
-        uploadStream: {},
-        delete: {},
-      },
-    },
-  },
-});
-
-export default ({ env }) => ({
-  upload: {
-    config: {
-      provider: 'cloudinary',
-      providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
-      },
-      actionOptions: {
         upload: {
-           resource_type: "auto",  // <--- Importante para PDFs
-           access_mode: "public"   // <--- FUERZA QUE SEA PÚBLICO
+          resource_type: "auto",
+          access_mode: "public"
         },
         uploadStream: {},
         delete: {},
