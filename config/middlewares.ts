d41ecda9e9b921bb708bb16cbc3532fd43ee78e1
Raul Nicolas <dtower79@gmail.com>
2026-05-18 01:19:59 +0200

    Fix: Configurada CSP para permitir CKEditor

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -3,7 +3,36 @@ import type { Core } from '@strapi/strapi';
 const config: Core.Config.Middlewares = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          // Permitimos que CKEditor inyecte sus estilos y scripts necesarios
+          'script-src': ["'self'", "'unsafe-inline'", 'cdn.ckeditor.com'],
+          'style-src': ["'self'", "'unsafe-inline'"],
+          // Permitimos iconos (data:) y previsualizaciones (blob:) además de Cloudinary
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'res.cloudinary.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'res.cloudinary.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
